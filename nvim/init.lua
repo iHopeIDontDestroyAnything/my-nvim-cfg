2ee4a9ca0ac0adab2116be0999112f603720b838
iHopeIDontDestroyAnything <jakub.urban62@gmail.com>
2025-02-07 21:16:36 +0100

    Delete nvim directory

--- nvim/init.lua
+++
@@ -1,6 +0,0 @@
-dofile("/home/jakub/.config/nvim/core/options.lua")
---constant path for options.lua so it is loaded outside config directory
-
-dofile("/home/jakub/.config/nvim/core/keymaps.lua")
---path for keymap settings
-
