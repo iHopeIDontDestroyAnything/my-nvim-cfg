9383bee4aa7b3b3b20ddb8e5d4dfc914eed4fc8b
iHopeIDontDestroyAnything <jakub.urban62@gmail.com>
2025-03-28 20:54:28 +0100

    Update nvim-cmp.lua

--- nvim/lua/jakub/plugins/nvim-cmp.lua
+++ nvim/lua/jakub/plugins/nvim-cmp.lua
@@ -45,9 +45,9 @@ return {
       }),
       -- sources for autocompletion
       sources = cmp.config.sources({
-        { name = "nvim_lsp"},
+        { name = "nvim_lsp", keyword_length = 3},
         { name = "luasnip" }, -- snippets
-        { name = "buffer" }, -- text within current buffer
+        { name = "buffer", keyword_length = 3}, -- text within current buffer
         { name = "path" }, -- file system paths
       }),
 
