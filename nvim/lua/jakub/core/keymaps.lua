9b0e8450f3c9b23a04c03b57f82835dfd4741d4b
iHopeIDontDestroyAnything <jakub.urban62@gmail.com>
2025-03-29 19:32:09 +0100

    Update keymaps.lua

--- nvim/lua/jakub/core/keymaps.lua
+++ nvim/lua/jakub/core/keymaps.lua
@@ -44,12 +44,12 @@ keymap.set("t","<C-l>","<RIGHT>", {desc = "ctrl h in terminal mode to move right
 keymap.set("t","<C-k>","<UP>", {desc = "ctrl h in terminal mode to move up"})
 keymap.set("t","<C-j>","<DOWN>", {desc = "ctrl h in terminal mode to move down"})
 
---browser keymaps (nvim-tree)
-keymap.set("n","<leader>bb","<cmd>NvimTreeToggle<CR>", {desc = "Toggle browser"})
-keymap.set("n","<leader>bc","<cmd>NvimTreeCollapse<CR>", {desc = "Collapse browser"})
-keymap.set("n","<leader>br","<cmd>NvimTreeRefresh<CR>", {desc = "Refresh browser"})
-keymap.set("n","<leader>bh","<cmd>NvimTreeFindFileToggle<CR>", {desc = "Toggle browser here (on current file)"})
-
+--browser
+--keymap.set("n","<leader>bb","<cmd>NvimTreeToggle<CR>", {desc = "Toggle browser"})
+--keymap.set("n","<leader>bc","<cmd>NvimTreeCollapse<CR>", {desc = "Collapse browser"})
+--keymap.set("n","<leader>br","<cmd>NvimTreeRefresh<CR>", {desc = "Refresh browser"})
+--keymap.set("n","<leader>bh","<cmd>NvimTreeFindFileToggle<CR>", {desc = "Toggle browser here (on current file)"})
+keymap.set("n","<leader>b","<cmd>Ex<CR>")
 --tab keymaps
 keymap.set("n","<leader>to","<cmd>tabnew<CR>", {desc = "open new tab"})
 keymap.set("n","<leader>tx","<cmd>tabclose<CR>", {desc = "close current tab"})
